149fd24a19c982f818ca2989c58c61e29b7ccb2d
qaq <haibucuo@faf.com>
2022-09-21 11:21:38 +0800

    push commit

--- src/main/java/git_test.java
+++ src/main/java/git_test.java
@@ -2,5 +2,6 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello git ");
         System.out.println("hello git  hot fix");
+        System.out.println("push test");
     }
 }
